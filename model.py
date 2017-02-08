28a1e2f44881c26d63e4d5b7ed1e222bae31e808
vagrant <vagrant@vagrant.vm>
2017-02-08 01:25:15 +0000

    continuing seed functionality - adding comics.

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-"""
+"""all classes for interaction for the database.
 """
 
 from flask_sqlalchemy import SQLAlchemy 
@@ -12,15 +12,83 @@ class Movie(db.Model):
 
     __tablename__ = "movies"
 
-    tmdb_id = db.Column(db.Integer, primary_key=True)
-    imdb_id = db.Column(db.  )
-    overview = db.Column(db.  )
-    tagline = db.Column(db.  )
-    image_src = db.Column(db.  )
-    title = db.Column(db.  )
-    tmdb_rating = db.Column(db.  )
-    tmdb_genre_list = db.Column(db.  )
-    bechdel_rating = db.Column(db.  )
+    tmdb_id = db.Column(db.String(12), primary_key=True)
+    imdb_id = db.Column(db.String(20))
+    overview = db.Column(db.UnicodeText) #blob???
+    tagline = db.Column(db.String(200))
+    image_src = db.Column(db.String(200))
+    title = db.Column(db.String(200))
+    tmdb_rating = db.Column(db.Numeric(asdecimal=True))
+    tmdb_genre_list = db.Column(db.  ) ###?
+    bechdel_rating = db.Column(db.Integer)
+
+    # manually coded categories
+
+    def __repr__():
+        pass
+
+
+class Comic(db.Model):
+    """class for comic info
+    """
+
+    __tablename__ = "comics"
+
+    isbn10 = db.Column(db.String(10), primary_key=True)
+    isbn13 = db.Column(db.String(13), unique=True)
+    author_list = db.Column(db.String(100))#
+    #artist?
+    publisher = db.Column(db.String(50))
+    title = db.Column(db.String(200))
+    summary = db.Column(db.UnicodeText)
+
+    # manually coded categories
+
+    def __repr__():
+        pass
+
+class User(db.Model):
+    """class for user info
+    """
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(50))
+    password = db.Column(db.String(12))
+
+    # calculated preferences for profile
+
+    def __repr__():
+        pass
+
+class MovieRating(db.Model):
+    """collects the rating for each movie the user rates (1-5)
+    """
+
+    __tablename__ = "movie_ratings"  
+
+    mr_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, ForeignKey(users.user_id), nullable=False)
+    tmdb_id = db.Column(db.String(12), ForeignKey(movies.tmdb_id), nullable=False)
+    rating = db.Column(db.Integer, nullable=False) # 1-5
+    # date?
+
+    def __repr__():
+        pass
+
+class ComicRec(db.Model):
+    """class for comic recommendations to each user.
+    """
+
+    __tablename__ = "comic_recs"
+
+    cr_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, ForeignKey(users.user_id), nullable=False)
+    isbn10 = db.Column(db.String(12), ForeignKey(comics.isbn10), nullable=False)
+    recommended_at = db.Column(db.Date)
+    user_read = db.Column(Boolean)
+    user_rating = db.Column(db.Integer)  # 1-5
 
     def __repr__():
         pass
--- seed.py
+++ seed.py
@@ -1,19 +1,22 @@
+"""collect and format data to populate the database.
+"""
+
 import json
 import requests
 import isbnlib
 import sys
 import os
 
-# pull api keys from environ (created form secrets file)
+# pull api keys from environ (created from secrets file)
 tmdb_key = os.environ["TMDB_KEY"]
 tmdb_read = os.environ["TMDB_READ_TOKEN"]
 isbn_key = os.environ["ISBN_KEY"]
 
 
 def read_movielist(filename):
-    """opens named file, returns a dicitonary of all titles (key)
+    """opens named file, returns a dictionary of all titles (key)
         & TMDB ids (value)
-        e.g. Jack+Reacher|999
+        input e.g. Jack+Reacher|999
     """
     movie_ids = {}
 
@@ -41,7 +44,7 @@ def get_movie_tmdb(movie_id):
 def get_bechdel_score(imdb_id):
     """movies only! pass in imdb_id as string, returns score as string.
     """
-    # bechdel site requires the leading r letters be dropped
+    # bechdel site requires the leading 2 letters be dropped
     bechdel_id = imdb_id[2:]
 
     bechdel_by_id = "http://bechdeltest.com/api/v1/getMovieByImdbId?imdbid=" + bechdel_id
@@ -53,8 +56,9 @@ def get_bechdel_score(imdb_id):
     return bechdel_score
 
 
+#bring over manual ratings too?  or handle separately? from read file...
 def process_movie_list(movie_ids):
-    """takes a dictionary of title[tmdb_id],
+    """takes a dictionary of title: tmdb_id,
     collects tmdb data on it (get_movie_tmdb),
     gleans valued data from the entire set,
     returns a list of dictionaries, ready for the db.
@@ -75,43 +79,86 @@ def process_movie_list(movie_ids):
         movie["title"] = tmdb["title"]
         movie["tmdb_rating"] = tmdb["vote_average"]
         movie["tmdb_genre_list"] = tmdb["genres"]
+        # MPAA rating????
 
+        # add on bechdel rating for each movie
         movie["bechdel_rating"] = get_bechdel_score(movie["imdb_id"])
 
         movies.append(movie)
 
-    # LIST of all data re
+    # LIST of all downloadable data on each movie as dictionary
     return movies
 
+##########################
+# Books
 
-# https://api.themoviedb.org/3/search/movie?api_key=e3d0fc958b1142490f250b376de9e820&query=Jack+Reacher
-# https://api.themoviedb.org/3/search/movie?api_key=e3d0fc958b1142490f250b376de9e820&query=Jack+Reacher
+def read_comiclist(filename):
+    """opens named file, returns a dicitionary of all titles (key)
+        & value of a tuple with both ISBN10 & ISBN13
+        input e.g. Nao+of+Brown|1906838429|9781906838423
+    """
+    comics_ids = {}
+
+    with open(filename) as comic_file:
+        for comic in comic_file:
+            comic = comic.rstrip()
+            title, ISBN10, ISBN13 = comic.split("|")
+            comics_ids[title] = (ISBN10, ISBN13)
+
+    return comics_ids
+
+#  https://www.comics.org/ ?????
+def get_comic_by_isbn(isbn):
+    """takes an isbn id string (numbers as string), sends a request to isbndb,
+        converts each json response to a dictionary and returns it.
+    """
+
+    isbn_request = "http://isbndb.com/api/v2/json/" + isbn_key + "/book/" + isbn 
+    r = requests.get(isbn_request)
+    comic = r.json()
+    comic_data = comic['data'][0]
+
+    return comic_data
 
 
-# example API request TMDB:
-# https://api.themoviedb.org/3/movie/343611?api_key={api_key}  ---search by movie id
-# Our current limits are 40 requests every 10 seconds
-# https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher search by title
+def process_comic_list(comics_ids):
+    """takes a dictionary of title:(ISBN10,ISBN13),
+    collects ISBN data on it (get_comic_by_isbn),
+    gleans valued data from the entire set,
+    returns a list of dictionaries, ready for the db.
+    """
 
-# borrowed code for OMDB  ... need to address titles with spaces!!!
+    comics = []
 
-#### for single request:
-# import json, requests
-# url = "http://www.omdbapi.com/?t=scream"
-# response = requests.get(url)
-# python_dictionary_values = json.loads(response.text)
+    for ctitle, ISBNs in comics_ids.items():
+        ISBN10 = ISBNs[0]
+        isbn = get_comic_by_isbn(ISBN10)
+
+        comic = {}
+
+        comic["isbn10"] = isbn["isbn10"]
+        comic["isbn13"] = isbn["isbn13"]
+        comic["author_list"] = isbn["author_data"]#   [0].get("name")
+        #artist?
+        comic["publisher"] = isbn["publisher_name"]
+        comic["title"] = isbn["title"]
+        comic["summary"] = isbn["summary"]  # figure out these paths??
+
+        comics.append(comic)
+
+    # LIST of all downloadable data on each comic as dictionary
+    return comics
 
 
-#### for multiple movies from file:
-# movies = {}
-# import json, requests
-# baseurl = "http://omdbapi.com/?t=" #only submitting the title parameter
-# with open("movies.txt", "r") as fin:
-#      for line in fin:
-#          movieTitle = line.rstrip("\n") # get rid of newline characters
-#          response = requests.get(url + movieTitle)
-#          if response.status_code == 200:
-#               movies[movieTitle] = json.loads(response.text)
-#          else:
-#               raise ValueError("Bad request!")
-# print movies['scream']
+# if __name__ == '__main__':
+
+#     movie_file = sys.args[1]
+#     comic_file = sys.args[2]
+
+#     movie_list = read_movielist(movies)
+#     movies = process_movie_list(movie_list)
+
+#     comic_list = read_comiclist(comic_file)
+#     comics = process_comic_list(comic_list)
+
+    # instantiate each list on their class and add to db
