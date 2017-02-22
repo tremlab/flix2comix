e0c7635864a1881b65104801b8165c8875aaacd0
vagrant <vagrant@vagrant.vm>
2017-02-22 20:17:11 +0000

    adding JavaScript. form validation for registration. hide/show login & registration, rather than separate pages.  Closes #11, #9, #8 issues.

--- server.py
+++ server.py
@@ -25,10 +25,6 @@ def index():
 
     return render_template("homepage.html")
 
-@app.route('/register', methods=["GET"])
-def display_register_form():
-
-    return render_template("register_form.html")
 
 @app.route('/register', methods=["POST"])
 def process_register_form():
@@ -40,7 +36,7 @@ def process_register_form():
     try:
         user_check = User.query.filter_by(email=email).one()
         flash("Email already registered!")
-        return render_template("register_form.html")
+        return render_template("/")
     except NoResultFound:
         user = User(email=email,
                     password=password,
@@ -50,15 +46,15 @@ def process_register_form():
 
         session['user_id'] = user.user_id
         flash("You are registered & logged in.")
-        return redirect("/movies")
+        return redirect("/")
 
 
-@app.route('/login', methods=["GET"])
-def display_login_form():
-    """show the login form for exisitng users
-    """
+# @app.route('/login', methods=["GET"])
+# def display_login_form():
+#     """show the login form for exisitng users
+#     """
 
-    return render_template("login_form.html")
+#     return render_template("login_form.html")
 
 
 @app.route('/login-validation', methods=["POST"])
@@ -78,10 +74,10 @@ def process_login_form():
     if password == user_check.password:
         session['user_id'] = user_check.user_id
         flash("Logged in successfully")
-        return redirect("/movies")
+        return redirect("/")
     else:
         flash("Wrong password!")
-        return render_template("login_form.html")
+        return render_template("/")
 
 
 @app.route('/logout')
@@ -211,3 +207,13 @@ if __name__ == '__main__':
     DebugToolbarExtension(app)
 
     app.run(port=5000, host='0.0.0.0')
+
+
+
+
+###################333
+#  hide/show in page...
+# @app.route('/register', methods=["GET"])
+# def display_register_form():
+
+#     return render_template("register_form.html")
--- static/js/flix2comics.js
+++ static/js/flix2comics.js
@@ -1,35 +1,92 @@
 // flix2comix.js
+"use strict"
 
+document.addEventListener ("DOMContentLoaded", function() 
+    {
+        $('input').on('focus', fHandleEnter);   // toggle?
+        $('input').on('blur', fHandleExit);
+        $('input').on('blur', fEnableSubmit);
 
-// $( function() {
-//     // onload....
-//     // all event listeners here
-    $('input').on('focus', highlight);
-    $('input').on('blur', unhighlight);
+        $('#signUpButton').on('click', fDisplayRegForm);
+        $('#cancelReg').on('click', fHideRegForm);
+        $('#loginButton').on('click', fDisplayLoginForm);
+        $('#cancelLogin').on('click', fHideLoginForm);
 
-    $('#password2').on('blur', passwordsMatch);
-
-
-// }
+        $('#email').on('blur', fValidateEmail);
+        $('#password2').on('blur', fPasswordsMatch);
+    } // closes anon function
+); // closes DOM event listener
 
 console.log("connected!!!!!!!");
 
-function highlight(evt) {
+function fHandleEnter(evt) {
     $(this).addClass("highlight");
 }
 
-function unhighlight(evt) {
+function fHandleExit(evt) {
     $(this).removeClass("highlight");
 }
 
-function passwordsMatch(evt) {
+function fDisplayRegForm(evt) {
+    $('#registerForm').removeClass('hidden');
+    $('#notSignedIn').hide();
+    console.log("clicky reg");
+}
+
+function fHideRegForm(evt) {
+    $('#registerForm').addClass('hidden');
+    $('#notSignedIn').show();
+    console.log("clicky cancel");
+}
+
+function fDisplayLoginForm(evt) {
+    $('#loginForm').removeClass('hidden');
+    $('#notSignedIn').hide();
+    console.log("clicky login");
+}
+
+function fHideLoginForm(evt) {
+    $('#loginForm').addClass('hidden');
+    $('#notSignedIn').show();
+    console.log("clicky cancel login");
+}
+
+function fPasswordsMatch(evt) {
     if ($('#password').val() === $('#password2').val()) {
         $('#passMatch').empty();        
     }
 
     else {
-        $('#passMatch').html('<p>passwords do not match. :(</p>')
+        $('#passMatch').html('<p>passwords do not match. :(</p>');
     }
+}
+// TEST!!!!!!!!!!!!!!!
+function fValidateEmail(evt) {
+    var emailInput = $('#email').val();
+    if (emailInput.length > 0) {
+        var emailRegEx = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if (emailRegEx.test(emailInput)) {
+            $('#emailValid').empty();        
+        }
+
+        else {
+            $('#emailValid').html('<p>please enter a valid email. :(</p>');
+        }
+    }
+}
+
+
+function fEnableSubmit(evt) {
+    if (
+        $('#email').val().length > 0 && 
+        $('#password').val().length > 0 &&
+        //$('#emailValid').length == 0 &&
+        $('#password').val() === $('#password2').val()
+    )
+        {
+        $('#registerButton').prop('disabled', false);
+        }
+
 }
 
 // function isNotEmpty(inputElm, errMsg, errElm) {
